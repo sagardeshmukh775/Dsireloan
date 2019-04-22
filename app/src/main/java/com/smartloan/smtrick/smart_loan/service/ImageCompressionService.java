8e527da9a0d168b4c02061bdeea4ac02ea7b40d2
Emma Paris <eparis@atlassian.com>
2019-04-22 18:11:35 +0530

    updated on 22.04.2019

--- app/src/main/java/com/smartloan/smtrick/smart_loan/service/ImageCompressionService.java
+++
@@ -1,8 +0,0 @@
-package com.smartloan.smtrick.smart_loan.service;
-
-
-import com.smartloan.smtrick.smart_loan.callback.CallBack;
-
-public interface ImageCompressionService {
-    void compressImage(String ImagePath, CallBack callBack);
-}
